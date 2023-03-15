25c9170749661b5ffe2fe60f2ac35d4f670449eb
Str4n1k <glebus.i@mail.ru>
2023-03-15 11:54:25 +0700

    XOR

--- HomeWork8/main.cpp
+++ HomeWork8/main.cpp
@@ -4,40 +4,15 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-
-int Factorial(int n);
-double Power(double a, int n);
 void main()
 {
 	setlocale(LC_ALL, "");
-	int a, b;
-	cout << "Введите два числа: "; cin >> a >> b;
-
-	cout << a << " ^ " << b << " = " << Power(a, b) << endl;
-	int n;
-	cout << "Введите число для вычисления факториала: "; cin >> n;
-	cout << n << "! = " << Factorial(n) << endl;
-}
-int Factorial(int n)
-{
-	int f = 1;
-	for (int i = 1; i <= n; i++)
-	{
-		f *= i;
-	}
-	return f;
-}
-double Power(double a, int n)
-{
-	double N = 1;
-	if (n < 0)
-	{
-		a = 1 / a;
-		n = -n;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		N *= a;
-	}
-	return N;
+	int a;
+	int b;
+	cout << "Введите первую переменную: " << endl; cin >> a;
+	cout << "Введите вторую переменную: " << endl; cin >> b;
+	a = a ^ b;
+	b = a ^ b;
+	a ^= b;
+	cout << a << " " << b;
 }
